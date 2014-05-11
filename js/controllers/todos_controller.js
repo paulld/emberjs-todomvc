4949f2c5a3f3050b5316d2edacc0caedb0247f56
paul-ld <paul.lugagnedelpon@gmail.com>
2014-05-11 23:03:16 +0800

    Todo MVC is complete

--- js/controllers/todos_controller.js
+++ js/controllers/todos_controller.js
@@ -1,5 +1,10 @@
 Todos.TodosController = Ember.ArrayController.extend({
   actions: {
+    clearCompleted: function() {
+      var completed = this.filterBy('isCompleted', true);
+      completed.invoke('deleteRecord');
+      completed.invoke('save');
+    },
     createTodo: function() {
       // Get the todo title set by the "New Todo" text field
       var title = this.get('newTitle');
@@ -19,6 +24,14 @@ Todos.TodosController = Ember.ArrayController.extend({
     }
   },
 
+  hasCompleted: function() {
+    return this.get('completed') > 0;
+  }.property('completed'),
+
+  completed: function() {
+    return this.filterBy('isCompleted', true).get('length');
+  }.property('@each.isCompleted'),
+
   remaining: function() {
     return this.filterBy('isCompleted', false).get('length');
   }.property('@each.isCompleted'),
@@ -26,6 +39,16 @@ Todos.TodosController = Ember.ArrayController.extend({
   inflection: function() {
     var remaining = this.get('remaining');
     return remaining === 1 ? 'todo' : 'todos';
-  }.property('remaining')
+  }.property('remaining'),
+
+allAreDone: function(key, value) {
+  if (value === undefined) {
+    return !!this.get('length') && this.everyProperty('isCompleted', true);
+  } else {
+    this.setEach('isCompleted', value);
+    this.invoke('save');
+    return value;
+  }
+}.property('@each.isCompleted')
 
 });
